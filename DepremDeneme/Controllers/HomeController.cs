696a18963f9bcbfef8a19c2656d3fd1e61fa37b9
ZelihaOrhann <zeliha.social1@gmail.com>
2024-11-11 13:11:28 +0300

    Add project files.

---
+++ DepremDeneme/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using DepremDeneme.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using DepremDeneme.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class HomeController : Controller
+{
+	private readonly HttpClient _httpClient;
+
+	public HomeController()
+	{
+		_httpClient = new HttpClient();
+	}
+
+	// Türkiye genelinde son 24 saatteki depremleri çeker
+	public async Task<IActionResult> Index()
+	{
+		string apiUrl = "https://api.orhanaydogdu.com.tr/deprem/kandilli/live"; // API URL
+
+		var response = await _httpClient.GetStringAsync(apiUrl);
+		var apiResponse = JsonConvert.DeserializeObject<ApiResponse>(response);
+
+		return View(apiResponse.Data);
+	}
+}
+
+// API'den dönen JSON yapısına uygun model
+public class ApiResponse
+{
+	public List<Deprem> Data { get; set; }
+}
---
+++ DepremDeneme/Models/Deprem.cs
@@ -0,0 +1,11 @@
+﻿namespace DepremDeneme.Models
+{
+	public class Deprem
+	{
+		public string Tarih { get; set; }
+		public double Buyukluk { get; set; }
+		public string Yer { get; set; }
+		public double Enlem { get; set; }
+		public double Boylam { get; set; }
+	}
+}
